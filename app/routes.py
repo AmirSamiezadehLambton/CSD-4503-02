00e1f4deef3d2067bb4f9f0948f978797dad2afb
Amir Samiezadeh <amir.s@orthobiomed.ca>
2024-09-25 11:21:17 -0400

    add render_template to read index.html for the landing page

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
 from app import app
+from flask import render_template
 
 
 @app.route("/index", methods=["GET"])
 def index():
     return "This is an awesome app!"
+
+
+@app.route("/", methods=["GET"])
+def landing():
+    return render_template("index.html")
