2598a84fdcbd1878a3590a74f92822121f2274f3
Amir Samiezadeh <amir.s@orthobiomed.ca>
2024-10-09 10:54:42 -0400

    add .gitignore for .env and reading db secrets from .env file

--- app/__init__.py
+++ app/__init__.py
@@ -4,11 +4,19 @@
 # To import the flask framework, we use:
 from flask import Flask
 from pymongo import MongoClient  # pip install pymongo
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+db_username = os.environ["MONGODB_USERNAME"]
+db_password = os.environ["MONGODB_PASSWORD"]
+
+print(db_username)
 app = Flask(__name__)
 
 # MongoDB Atlas Client
-client = MongoClient("mongodb+srv://root:dBAxAFAZS3t43anh@cluster0.ktsh8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient(f"mongodb+srv://{db_username}:{db_password}@cluster0.ktsh8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 app_db = client.app  # app is the name of the database in MongoDB Atlas
 products_collection = app_db.products  # products is the name of the collection inside the database
